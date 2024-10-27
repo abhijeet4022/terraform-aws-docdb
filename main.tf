6e883d24f1f6b65b19a8154e6ea292d9d19005fc
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-27 09:34:45 +0530

    Project Infra

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Create the subnet group for documentdb
 resource "aws_docdb_subnet_group" "main" {
-  name       = "${local.name_prefix}-sg"
+  name       = "${local.name_prefix}-subnet-group"
   subnet_ids = var.db_subnets
-  tags       = merge(local.tags, { Name = "${local.name_prefix}-sg" })
+  tags       = merge(local.tags, { Name = "${local.name_prefix}-subnet-group" })
 }
 
 
@@ -14,5 +14,34 @@ resource "aws_docdb_cluster_parameter_group" "main" {
   tags        = merge(local.tags, { Name = "${local.name_prefix}-pg" })
 
 }
+
+
+# SG for DocumentDB.
+resource "aws_security_group" "docdb_sg" {
+  name        = "${local.name_prefix}-sg"
+  description = "${local.name_prefix}-sg"
+  vpc_id      = var.vpc_id
+  tags        = merge(var.tags, { Name = "${local.name_prefix}-sg" })
+}
+
+
+# Ingress rule for ALB SG.
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  for_each          = toset(var.app_subnets_cidr) # Convert list to a set to iterate over each CIDR
+  security_group_id = aws_security_group.docdb_sg.id
+  cidr_ipv4         = each.value  # Each CIDR block as a separate rule
+  from_port         = 27017
+  ip_protocol       = "tcp"
+  to_port           = 27017
+}
+
+
+# Egress rule for ALB SG.
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.docdb_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
 
 
--- vars.tf
+++ vars.tf
@@ -1,4 +1,14 @@
 variable "tags" {}
 variable "env" {}
-variable "db_subnets" {}
-variable "engine_family" {}
+variable "db_subnets" {
+  description = "Subnets ID to create the DB"
+}
+variable "engine_family" {
+  description = "DocumentDB Engine Version that we ant to create"
+}
+variable "vpc_id" {
+  description = "Pass the private VPC ID To create the SG in private VPC"
+}
+variable "app_subnets_cidr" {
+  description = "Pass the APP subnets CIDR to access the DB from App Subnets"
+}
