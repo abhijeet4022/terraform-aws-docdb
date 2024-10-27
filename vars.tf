72f0b0ffc979401ffdaab565d29fc97ebb2d3838
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-27 09:50:59 +0530

    Project Infra

--- vars.tf
+++ vars.tf
@@ -1,14 +1,23 @@
-variable "tags" {}
-variable "env" {}
+variable "tags" {
+  description = "A map of tags to assign to resources for identification and metadata."
+}
+
+variable "env" {
+  description = "The environment in which the DocumentDB instance will be deployed, e.g., 'dev', 'staging', or 'production'."
+}
+
 variable "db_subnets" {
-  description = "Subnets ID to create the DB"
+  description = "A list of subnet IDs where the DocumentDB instance will be deployed, ensuring access control and availability."
 }
+
 variable "engine_family" {
-  description = "DocumentDB Engine Version that we ant to create"
+  description = "The version of the DocumentDB engine to deploy, such as '3.6' or '4.0'."
 }
+
 variable "vpc_id" {
-  description = "Pass the private VPC ID To create the SG in private VPC"
+  description = "The ID of the private VPC where the security group will be created."
 }
+
 variable "app_subnets_cidr" {
-  description = "Pass the APP subnets CIDR to access the DB from App Subnets"
+  description = "A list of CIDR blocks for application subnets allowed to access the DocumentDB instance."
 }
