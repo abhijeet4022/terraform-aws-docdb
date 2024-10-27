db7383d0bb097afdffe9733dd51a664a079991a6
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-27 10:15:07 +0530

    Project Infra

--- main.tf
+++ main.tf
@@ -25,23 +25,39 @@ resource "aws_security_group" "docdb_sg" {
 }
 
 
-# Ingress rule for ALB SG.
+# Ingress rule for DocDB SG.
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   for_each          = toset(var.app_subnets_cidr) # Convert list to a set to iterate over each CIDR
   security_group_id = aws_security_group.docdb_sg.id
-  cidr_ipv4         = each.value  # Each CIDR block as a separate rule
+  cidr_ipv4         = each.value # Each CIDR block as a separate rule
   from_port         = 27017
-  ip_protocol       = "tcp"
   to_port           = 27017
+  ip_protocol       = "tcp"
 }
 
 
-# Egress rule for ALB SG.
+# Egress rule for DocDB SG.
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.docdb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
+
+# DocumentDB Database Cluster
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier              = "${local.name_prefix}-cluster"
+  engine                          = "docdb"
+  engine_version                  = var.engine_version
+  db_subnet_group_name            = aws_docdb_subnet_group.main.name
+  db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.main.name
+  vpc_security_group_ids          = [aws_security_group.docdb_sg.id]
+  master_username                 = "foo"
+  master_password                 = "mustbeeightchars"
+  backup_retention_period         = var.backup_retention_period
+  preferred_backup_window         = var.preferred_backup_window
+  skip_final_snapshot             = var.skip_final_snapshot
+  tags                            = merge(local.tags, { Name = "${local.name_prefix}-cluster" })
+}
 
 
--- vars.tf
+++ vars.tf
@@ -6,6 +6,7 @@ variable "env" {
   description = "The environment in which the DocumentDB instance will be deployed, e.g., 'dev', 'staging', or 'production'."
 }
 
+# DocDB PG
 variable "db_subnets" {
   description = "A list of subnet IDs where the DocumentDB instance will be deployed, ensuring access control and availability."
 }
@@ -14,10 +15,29 @@ variable "engine_family" {
   description = "The version of the DocumentDB engine to deploy, such as '3.6' or '4.0'."
 }
 
+# DocDB SG
 variable "vpc_id" {
   description = "The ID of the private VPC where the security group will be created."
 }
 
 variable "app_subnets_cidr" {
   description = "A list of CIDR blocks for application subnets allowed to access the DocumentDB instance."
+}
+
+
+# DocDB Cluster
+variable "backup_retention_period" {
+  description = "The number of days to retain backups for the DocumentDB cluster, with a minimum of 1 and a maximum of 35 days."
+}
+
+variable "preferred_backup_window" {
+  description = "The preferred daily time range for automatic backups, specified in UTC (e.g., '07:00-09:00')."
+}
+
+variable "skip_final_snapshot" {
+  description = "A boolean that determines whether to skip the creation of a final snapshot when the cluster is deleted. Set to 'true' to skip, or 'false' to create a final snapshot."
+}
+
+variable "engine_version" {
+  description = "The version of the DocumentDB engine to use for the cluster, e.g., '3.6' or '4.0'."
 }
